952ef2c68c0c427c2a621cb700bee42dd7925f1b
NawafAlGhodyah <nwaf557@gmail.com>
2024-03-26 00:27:01 +0300

    setting up the ViewModel

---
+++ app/src/main/java/com/example/recipe/ApiService.kt
@@ -0,0 +1,15 @@
+package com.example.recipe
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+private val _retrofit = Retrofit.Builder().baseUrl("www.themealdb.com/api/json/v1/1/")
+    .addConverterFactory(GsonConverterFactory.create()).build()
+
+val recipeService = _retrofit.create(ApiService::class.java)
+
+interface ApiService {
+    @GET("categories.php") //here we put what part of our baseURL we will get
+    suspend fun getCategories():CategoriesResponse
+}
---
+++ app/src/main/java/com/example/recipe/MainViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.recipe
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _categoriesState = mutableStateOf(RecipeState())
+    val categoriesState : State<RecipeState> = _categoriesState //expose value only to external classes
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories(){
+        //this will allow suspend function to be implemented
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categoriesState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+
+            }catch (e: Exception){
+                // we use copy to make a list same as the original list but we override some of the values
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    error("Error fetching Categories : ${e.message}")
+                )
+            }
+        }
+    }
+
+    data class RecipeState(
+        val loading : Boolean = true,
+        val list : List<Category> = emptyList(),
+        val error: String? = null
+    )
+}
