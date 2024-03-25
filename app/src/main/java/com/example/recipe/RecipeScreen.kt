37a0577ae492b31cbe2c820a090725a42f3a3978
NawafAlGhodyah <nwaf557@gmail.com>
2024-03-26 01:11:14 +0300

    ui for how each item should look like

---
+++ app/src/main/java/com/example/recipe/RecipeScreen.kt
@@ -0,0 +1,82 @@
+package com.example.recipe
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier) {
+    val recipeViewModel : MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+    Box(modifier = Modifier.fillMaxSize()){
+        when{
+            //when the lis is loading
+            viewState.loading ->CircularProgressIndicator(modifier.align(Alignment.Center))
+
+            //when there is an error
+            viewState.error != null -> Text("Error Occurred")
+
+            //here we display the full list
+            else ->{
+                CategoryList(categories = viewState.list)
+            }
+        }
+    }
+}
+
+
+//the group of all items
+@Composable
+fun CategoryList(categories:List<Category>) {
+    LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) 
+    {
+        items(items = categories){
+            category ->
+            CategoryItem(category = category)
+        }
+    }
+}
+
+
+//the look of each item
+@Composable
+fun CategoryItem(category: Category) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb),
+            contentDescription = "Description Type : ${category.strCategory}",
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+        )
+        Text(
+            text =category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
+    }
+}
